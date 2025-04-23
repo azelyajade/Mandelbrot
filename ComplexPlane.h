6fc0ed2654bcbf65097d38ac80c37a73f309802d
azelyajade <zelibelimac@gmail.com>
2025-04-23 12:09:35 -0700

    Create ComplexPlane.h

---
+++ ComplexPlane.h
@@ -0,0 +1,43 @@
+// **ALL CAPS: TO DO
+// **lowercase: done
+
+// HEADER STUFF
+
+
+// constant values
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
+// ENUM CLASS (STATE) - CALC, DISPLAY
+
+class ComplexPlane : public Drawable
+{
+public: // -- CHECK
+	ComplexPlane(int pixelWidth, int pixelHeight);
+	void draw(RenderTarget& target, RenderStates states) const;
+	void updateRender();
+	void zoomIn();
+	void zoomOut();
+	void setCenter(Vector2i mousePixel);
+	void setMouseLocation(Vector2i mousPixel);
+	void loadText(Text& text);
+	
+	// CHECK RIGHT LOCTION? IN PRIVATE?
+	size_t countIterations(Vector2f coord);
+	void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+	Vector2f mapPixelToCoords(Vector2i mousePixel);
+
+private: 
+	// -- CHECK
+	VertexArray m_va;
+	State m_state;
+	Vector2f m_mouseLocation;
+	Vector2i m_pixel_size;
+	Vector2f m_plane_center;
+	Vector2f m_plane_size;
+	int m_zoomCount;
+	float m_aspectRatio;
+
+};

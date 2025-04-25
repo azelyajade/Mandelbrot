0a3f9c5f7c30ca3fe0e5e4f8a69c9e7a229e72d3
azelyajade <zelibelimac@gmail.com>
2025-04-24 21:20:56 -0700

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -8,21 +8,46 @@ ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {
 	m_plane_size = { BASE_WIDTH, BASE_HEIGHT * m_aspectRatio };
 	m_zoomCount = 0;
 
-	VertexArray m_va;
+	VertexArray m_va; // IS VertexArray NEEDED?
 	m_va.setPrimitiveType(Points);
 	m_va.resize(pixelWidth * pixelHeight);
 }
 
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
+	target.draw(m_va);
+}
+
+void ComplexPlane::updateRender() {
+	// TO DO
+}
+
+void ComplexPlane::zoomIn() {
+	m_zoomCount++;
+	float xVal = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+	float yVal = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+	m_plane_size = { xVal, yVal };
+	m_state = State::CALCULATING;
+}
+
+void ComplexPlane::zoomOut() {
+	m_zoomCount--;
+	float xVal = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+	float yVal = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+	m_plane_size = { xVal, yVal };
+	m_state = State::CALCULATING;
+}
+
+void ComplexPlane::setCenter(Vector2i mousePixel) {
 
 }
-void ComplexPlane::updateRender();
-void ComplexPlane::zoomIn();
-void ComplexPlane::zoomOut();
-void ComplexPlane::setCenter(Vector2i mousePixel);
-void ComplexPlane::setMouseLocation(Vector2i mousPixel);
+void ComplexPlane::setMouseLocation(Vector2i mousPixel) {
+	// !!! TO DO
+}
 void ComplexPlane::loadText(Text& text);
-
+// PRIVATE FUNCTIONS
 size_t ComplexPlane::countIterations(Vector2f coord);
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
-Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel);
+
+Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel) {
+	// !!! TO DO
+}

3a0b48132f5de3ed1319abe2f22809244bddfada
squidco <anthonypillow18@gmail.com>
2025-04-27 23:33:31 -0700

    Added handling for user interaction and drawing stuff

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // C++ libraries
 #include <SFML/Graphics.hpp>
+#include "ComplexPlane.h"
 #include <iostream>
 
 
@@ -16,9 +17,26 @@ int main()
 
 	VideoMode vm(pixelWidth, pixelHeight);
 
-	RenderWindow window(vm, "Rainbow Screen", Style::Default);
+	RenderWindow window(vm, "Mandlebrot", Style::Default);
 
+	RenderStates rStates;
 
+	ComplexPlane plane(pixelWidth, pixelHeight);
+
+	// Font
+	Font font;
+
+	if (!font.loadFromFile("./Fonts/BLACCS_.ttf"))
+	{
+		cout << "Uh oh" << endl;
+	}
+
+	// Text
+
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(10);
+	text.setStyle(Text::Regular);
 
 	while (window.isOpen())
 
@@ -29,6 +47,36 @@ int main()
 						INPUT
 		****************************************
 		*/
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Mouse events
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				// Gets mouse location and sets the center
+				Vector2i location(event.mouseButton.x, event.mouseButton.y);
+				plane.setCenter(location);
+
+				// Zoom in or out based on what button was clicked
+				if (event.mouseButton.button == sf::Mouse::Left)
+				{
+					std::cout << "the left button was pressed" << std::endl;
+
+					plane.zoomIn();
+				}
+				if (event.mouseButton.button == sf::Mouse::Right)
+				{
+					std::cout << "the right button was pressed" << std::endl;
+
+					plane.zoomOut();
+				}
+			}
+			if (event.type == Event::MouseMoved)
+			{
+				Vector2i location(event.mouseMove.x, event.mouseMove.y);
+				plane.setMouseLocation(location);
+			}
+		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
@@ -48,7 +96,9 @@ int main()
 		****************************************
 		*/
 		// updateRender
+		plane.updateRender();
 		// loadText
+		plane.loadText(text);
 
 		// END of UPDATE
 
@@ -61,6 +111,8 @@ int main()
 		****************************************
 		*/
 		// 
+		plane.draw(window, rStates);
+		window.draw(text);
 		// 
 		// END of DRAW
 
